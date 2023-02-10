e603179f47831f67c096c873f61ada615d16a31d
John <follyjohn@outlook.fr>
2023-02-10 10:02:01 +0100

    - improvement of perf

--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,6 @@ int main(int argc, char *argv[])
                 if(task!=""){
                     broker->publish(new HMessage(task, bkchannel), pair.second);
                     broker->notify();
-                    break;
                 }
 
             }
